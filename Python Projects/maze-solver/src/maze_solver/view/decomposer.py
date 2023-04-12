578988cbc59531c9c881b42d591ad4ffc3f627a0
d3h1 <denicabaravdic@gmail.com>
2023-04-12 15:23:45 -0700

    creating a decompose function

---
+++ Python Projects/maze-solver/src/maze_solver/view/decomposer.py
@@ -0,0 +1,15 @@
+from maze_solver.models.border import Border
+from maze_solver.view.primitives import ( Line, Point, Primitive )
+
+# We are trying to decompose the border into relevant geometric primitives that can be drawn
+def decompose(border: Border, top_left: Point, square_size: int) -> Primitive:
+    top_right: Point = top_left.translate(x = square_size)
+    bottom_right: Point = top_left.translate(x = square_size, y = square_size)
+    bottom_left: Point = top_left.translate(y = square_size)
+    
+    top = Line(top_left, top_right)
+    bottom = Line(bottom_left, bottom_right)
+    left = Line(top_left, bottom_left)
+    right = Line(top_right, bottom_right)
+    
+    
