b089c07885acc77ec06967e1f615f43745db7f09
denicabaravdic <deni@phntmlabs.com>
2023-04-11 19:58:53 -0700

    added genexps, assert funcs, a,d a validation sequence

--- Python Projects/maze-solver/src/maze_solver/models/maze.py
+++ Python Projects/maze-solver/src/maze_solver/models/maze.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 from typing import Iterator
 from functools import cached_property
+
+from maze_solver.models.role import Role
 from maze_solver.models.square import Square
 
 # Once again creating an immutable data class to ensure underlying tuple of Square remains unchanged once it is assigned
@@ -8,19 +10,44 @@ from maze_solver.models.square import Square
 class Maze:
     squares: tuple[Square, ...]
     
-    # Instances will cooperate with the for loop
-    def __iter__(self) -> Iterator[Square]:
-        return iter(self.squares)
-    
-    # Enables the square bracket notation for getting index of squares
-    def __getitem__(self, index: int) -> Square:
-        return self.squares[index]
-    
-    # Iterate over the maze to find the max cols and rows index with max(). We add 1 to highest index accounts 
+    # next() calls on GENERATOR EXPRESSIONS that filters squares by their role. Since they were validated, you can now assume that no exceptions will be raised
     @cached_property
-    def width(self):
-        return max(square.column for square in self) + 1
+    def entrance(self) -> Square:
+        return next(sq for sq in self if sq.role is Role.ENTRANCE)
     
     @cached_property
-    def height(self):
-        return max(square.row for square in self) + 1
+    def exit(self) -> Square:
+        return next(sq for sq in self if sq.role is Role.EXIT)
+    
+    def __post_init__(self) -> None:
+        validate_indices(self)
+        validate_rows_columns(self)
+        validate_entrance(self)
+        validate_exit(self)
+
+# ! BOTH FUNCTIONS USE ASSERT to raise assertion error whilst preventing maze from being created with invalid data found
+# Checks index prop of each square and if it fits into a continuous sequence of numbers that enumerate all the squares in the maze
+def validate_indices(maze: Maze) -> None:
+    assert [square.index for square in maze] == list(
+        range(len(maze.squares))
+    ), 'Wrong square index'
+    
+# Iterates over rows and columns of maze ensuring that attributes of corresponding square match up with currents of the loop
+def validate_rows_columns(maze: Maze) -> None:
+    for y in range(maze.height):
+        for x in range(maze.width):
+            square = maze[y * maze.width + 1]
+            assert square.row == y, 'Wrong square.row'
+            assert square.column == x, 'Wrong square.column'
+
+# These functions count number of squares whose role are either Entrance and Exit to verify only one of each
+def validate_entrance(maze: Maze) -> None:
+    assert 1 == sum (
+        1 for square in maze if square.role is Role.ENTRANCE
+    ), 'Must be exactly one entrance'
+    
+def validate_exit(maze: Maze) -> None:
+    assert 1 == sum (
+        1 for square in maze if square.role is Role.EXIT
+    ), 'Must be exactly one exit'
+    
