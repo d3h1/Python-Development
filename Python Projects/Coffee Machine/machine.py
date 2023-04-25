81b516e75286e62df64292e3fdd5e27af21b50a4
d3h1 <denicabaravdic@gmail.com>
2023-04-25 16:44:01 -0700

    defining functions

--- Python Projects/Coffee Machine/machine.py
+++ Python Projects/Coffee Machine/machine.py
@@ -2,3 +2,15 @@ import random
 import math
 from levels import espresso_water_level, latte_water_level, cappuccino_water_level, espresso_coffee_level, latte_coffee_level, cappuccino_coffee_level, espresso_milk_level, latte_milk_level, cappuccino_milk_level, machine_water_level, machine_coffee_level, machine_milk_level, penny, nickle, dime, quarter
 
+# We will make our drinks here
+def drink_maker():
+    ...
+
+# We will check ingredient levels here   
+def ingredient_levels():
+    print(f'Current Milk Levels: {}') 
+
+# We will dispense drinks and check money here
+def machine():
+    current_money = 0
+    
