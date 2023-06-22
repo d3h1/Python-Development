684e0ad619a12b4e614f1e58cd26d2fc6a646a8e
d3h1 <denicabaravdic@gmail.com>
2023-06-21 23:07:06 -0700

    finished reviewing big o

--- D&A/DataStructures/BigO-of-Lists.py
+++ D&A/DataStructures/BigO-of-Lists.py
@@ -1,14 +1,14 @@
 my_list = [11,3,33,7]
+print(my_list)
 
 # !These will be O(1) because it is happening at the end of the list
 # my_list.pop(3)
 # my_list.append(88)
 
+# !These will be O(n) because reindexing needs to occur at the beginning or middle of list
+# my_list.pop(0)
+# my_list.pop(1)
+# my_list.insert(0,10)
+# my_list.insert(1,'hi')
+
 print(my_list)
-
-# These will be O(n) because reindexing needs tp occur
-my_list.pop(0)
-my_list.insert(0,10)
-
-print(my_list)
-
