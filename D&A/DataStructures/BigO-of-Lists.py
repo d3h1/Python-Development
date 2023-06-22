10e25256d1fbcb0a61e703c40a282a25b0feff3c
d3h1 <denicabaravdic@gmail.com>
2023-06-21 22:46:54 -0700

    reindexing in lists

--- D&A/DataStructures/BigO-of-Lists.py
+++ D&A/DataStructures/BigO-of-Lists.py
@@ -1,2 +1,14 @@
 my_list = [11,3,33,7]
 
+# !These will be O(1) because it is happening at the end of the list
+# my_list.pop(3)
+# my_list.append(88)
+
+print(my_list)
+
+# These will be O(n) because reindexing needs tp occur
+my_list.pop(0)
+my_list.insert(0,10)
+
+print(my_list)
+
---
+++ D&A/DataStructures/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+my_list.pop(3)
+# my_list.insert(3, 88)
