16aa2a640a874934da1e06162bf42d88bf88c994
d3h1 <denicabaravdic@gmail.com>
2023-06-23 12:17:04 -0700

    classes done

--- D&A/DataStructures/Classes/ClassExample.py
+++ D&A/DataStructures/Classes/ClassExample.py
@@ -7,7 +7,14 @@ class Cookie:
     def get_color(self):
         return self.color
     
+    def set_color(self,color):
+        self.color = color
+    
     
 cookie_one = Cookie('red', 'chocolate', 'medium')
-
 print(f'Cookie ones color is {cookie_one.get_color()}')
+
+cookie_one.set_color('blue')
+print(f'Cookie ones color is {cookie_one.get_color()}')
+
+
---
+++ D&A/DataStructures/Classes/LinkedList.py
@@ -0,0 +1,18 @@
+class LinkedList:
+  def __init__ (self, value):
+    return 'test'
+  
+  def append (self, value):
+    return 'test'
+  
+  def prepend (self, value):
+    return 'test'
+  
+  def remove (self, index):
+    return 'test'
+  
+  def insert (self, index, value):
+    return 'test'
+  
+  # These are just certain things you would do within this class of Linked List and something we are gonna 
+  # create as we make and use more data structures
