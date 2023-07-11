e38128e260956af1ceba734f512b3603b8bf88e8
denicabaravdic <denicabaravdic@gmail.com>
2023-07-11 15:06:04 -0700

    doing some quick operator notes

---
+++ Python Help/operators.py
@@ -0,0 +1,8 @@
+# The result is floored meaning it will round to smallest integer
+print (43 // 10)
+
+# Modulo will give remainder
+print(10 % 2)
+print(-28 % 10)  # The remainder is positive if the right-hand operand is positive
+print(28 % -10)  # The remainder is negative if the right-hand operand is negative
+print(34.4 % 2.5)  # The remainder can be a float
