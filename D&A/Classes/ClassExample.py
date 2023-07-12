372496354dd900940789bcf2845848bc07b4d523
d3h1 <denicabaravdic@gmail.com>
2023-07-12 15:21:10 -0700

    finished calculator class

--- D&A/Classes/ClassExample.py
+++ D&A/Classes/ClassExample.py
@@ -36,26 +36,48 @@
 # Sum = Point(3,4,5)
 # print(Sum.sqSum())
 
-class Student:
-    def __init__(self, name, phy, chem, bio):
-        self.name = name
-        self.phy = phy
-        self.chem = chem
-        self.bio = bio
+# class Student:
+#     def __init__(self, name, phy, chem, bio):
+#         self.name = name
+#         self.phy = phy
+#         self.chem = chem
+#         self.bio = bio
     
-    def totalObtained(self):
-        return(self.phy + self.chem + self.bio)
+#     def totalObtained(self):
+#         return(self.phy + self.chem + self.bio)
         
     
-    def percentage(self):
-        return(self.totalObtained() / 300) * 100
+#     def percentage(self):
+#         return(self.totalObtained() / 300) * 100
         
         
     
 
-Mark = Student('Mark', 80, 50, 70)
+# Mark = Student('Mark', 80, 50, 70)
 
-print(Mark.totalObtained())
-print(Mark.percentage())
+# print(Mark.totalObtained())
+# print(Mark.percentage())
+
+class Calculator:
+    # Init Function
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+    # Calculator Operations
+    def add(self):
+        return(self.num1 + self.num2)
+    def subtract(self):
+        return(self.num2 - self.num1)
+    def multiply(self):
+        return(self.num1 * self.num2)
+    def divide(self):
+        return(self.num2 / self.num1)
+
+v1 = Calculator(4,3)
+
+print(v1.add())
+print(v1.subtract())
+print(v1.divide())
+print(v1.multiply())
 
 
