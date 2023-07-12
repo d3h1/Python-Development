367551f08b363d88b7b31059dafb73735cae787c
d3h1 <denicabaravdic@gmail.com>
2023-07-12 14:30:04 -0700

    trying new class

--- D&A/Classes/ClassExample.py
+++ D&A/Classes/ClassExample.py
@@ -1,20 +1,40 @@
-class Cookie:
-    def __init__ (self, color, taste, size):
-        self.color = color
-        self.taste = taste
-        self.size = size
+# class Cookie:
+#     def __init__ (self, color, taste, size):
+#         self.color = color
+#         self.taste = taste
+#         self.size = size
     
-    def get_color(self):
-        return self.color
+#     def get_color(self):
+#         return self.color
     
-    def set_color(self,color):
-        self.color = color
+#     def set_color(self,color):
+#         self.color = color
     
     
-cookie_one = Cookie('red', 'chocolate', 'medium')
-print(f'Cookie ones color is {cookie_one.get_color()}')
+# cookie_one = Cookie('red', 'chocolate', 'medium')
+# print(f'Cookie ones color is {cookie_one.get_color()}')
 
-cookie_one.set_color('blue')
-print(f'Cookie ones color is {cookie_one.get_color()}')
+# cookie_one.set_color('blue')
+# print(f'Cookie ones color is {cookie_one.get_color()}')
+
+
+class Point:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+    
+    def sqSum(self):
+        a = self.x ** 2
+        b = self.y ** 2
+        c = self.z ** 2
+        sum = a + b + c
+        return sum
+    
+        
+
+Sum = Point(3,4,5)
+Sum.a
+
 
 
